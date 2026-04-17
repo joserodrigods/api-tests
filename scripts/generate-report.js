879b4705f585ac08427ac083e26cbe6c395114e1
joserodrigods <joserodrigo@gec.inatel.br>
2026-04-17 08:16:31 -0300

    Add single-column layout for Cucumber report and update report generation script

--- scripts/generate-report.js
+++ scripts/generate-report.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const reporter = require('cucumber-html-reporter');
+
+const reportOutput = 'results/cucumber-report.html';
 
 reporter.generate({
   theme: 'bootstrap',
   jsonFile: 'results/cucumber-report.json',
-  output: 'results/cucumber-report.html',
+  output: reportOutput,
   reportSuiteAsScenarios: true,
   launchReport: false,
   metadata: {
@@ -11,3 +14,32 @@ reporter.generate({
     Executed: 'Local',
   },
 });
+
+const singleColumnFeaturesCss = `
+<style id="single-column-features">
+  .row > .feature-passed > .col-lg-6.col-md-6,
+  .row > .feature-failed > .col-lg-6.col-md-6,
+  .row > .feature-undefined > .col-lg-6.col-md-6 {
+    width: 100% !important;
+    max-width: 100% !important;
+    flex: 0 0 100% !important;
+    float: none !important;
+  }
+  .row > .feature-passed,
+  .row > .feature-failed,
+  .row > .feature-undefined {
+    width: 100%;
+    clear: both;
+  }
+</style>
+`;
+
+const reportHtml = fs.readFileSync(reportOutput, 'utf8');
+
+if (!reportHtml.includes('id="single-column-features"')) {
+  fs.writeFileSync(
+    reportOutput,
+    reportHtml.replace('</head>', `${singleColumnFeaturesCss}\n</head>`),
+    'utf8'
+  );
+}
